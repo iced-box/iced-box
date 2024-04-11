5cf13b4e6b72941370e844ac4324b92bc0fd8fee
cosmo <cosmo@ecwsa.com.br>
2024-04-11 00:04:09 -0300

    adicionado features na documentação

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+//! ### Features
+//! | Feature | Description |
+//! | --- | --- |
+//! | icon | Provides an icon pack |
+//! 
+//! To **install** a single feature, use ```cargo add iced-box --features icon```
+//!
+//! To **install** several features, use ```cargo add iced-box --features icon,widget```
+
+
 /// Provides an icon pack for simplified use
 ///
 pub mod icon {
