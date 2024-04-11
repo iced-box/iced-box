55b3d205bc4b23092ac1f4b83a64624baa16ee93
cosmo <cosmo@ecwsa.com.br>
2024-04-11 00:55:50 -0300

    fix

--- icon/src/lib.rs
+++
@@ -1,4 +0,0 @@
-mod lucide;
-
-/// A module that allows the use of [Lucide](https://lucide.dev/icons) icons in a simplified way, see all the icons that can be used on the official page <https://lucide.dev/icons>
-pub use lucide::Lucide;
